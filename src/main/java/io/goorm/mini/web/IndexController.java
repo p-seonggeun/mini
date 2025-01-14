68cb619e4e16f60347ddfaa1b2e4223a9fbd14ae
psg <1999byh@naver.com>
2025-01-14 11:53:57 +0900

    build: Thymeleaf 템플릿 엔진 의존성 추가

---
+++ src/main/java/io/goorm/mini/web/IndexController.java
@@ -0,0 +1,15 @@
+package io.goorm.mini.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model) throws Exception {
+
+        return "index";
+    }
+}
